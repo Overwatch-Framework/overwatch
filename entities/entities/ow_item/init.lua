7cf0196699211165b69f9ddc675049d6fb8533a4
eon (bloodycop) <minerva.servers.eon@gmail.com>
2025-01-26 00:32:09 +0200

    Update init.lua

--- entities/entities/ow_item/init.lua
+++ entities/entities/ow_item/init.lua
@@ -8,4 +8,21 @@ end
 
 function ENT:SetItem(uniqueID)
     local itemData = ow.item:Get(uniqueID)
+    if ( !itemData ) then return false end
+
+    self:SetModel(Model(itemData.Model))
+    self:SetSkin(itemData.Skin or 0)
+    self:SetColor(itemData.Color or color_white)
+    self:SetMaterial(itemData.Material or "")
+
+    for k, v in pairs(itemData.Bodygroups) do
+        if ( isstring(k) ) then
+            self:SetBodygroup(self:GetBodygroupByName(k), v)
+        elseif ( isnumber(k) ) then
+            self:SetBodygroup(k, v)
+        end
+    end
+    
+    -- bloodycop: Something like netvar or smth in the future
+    self:SetInternalVariable("m_iOWItemUniqueID", uniqueID)
 end
