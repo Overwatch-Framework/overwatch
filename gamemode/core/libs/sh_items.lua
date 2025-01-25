7505b320b89818d6e465fdfa8a9b7e6ef05d314e
eon (bloodycop) <minerva.servers.eon@gmail.com>
2025-01-26 00:13:45 +0200

    Update sh_items.lua

--- gamemode/core/libs/sh_items.lua
+++ gamemode/core/libs/sh_items.lua
@@ -8,7 +8,10 @@ ow.item.instances = {}
 function ow.item:Register(uniqueID, itemData)
     itemData.Name = itemData.Name or "Unknown Item"
     itemData.Description = itemData.Description or "No description provided."
+
     itemData.Model = itemData.Model or "models/props_junk/watermelon01.mdl"
+    util.PrecacheModel(itemData.Model)
+
     itemData.Stackable = itemData.Stackable or false
     itemData.MaxStack = itemData.MaxStack or 1
     itemData.Weight = itemData.Weight or 1
@@ -16,8 +19,6 @@ function ow.item:Register(uniqueID, itemData)
 
     self.instances[#self.instances + 1] = itemData
     self.stored[uniqueID] = itemData
-
-
 end
 
 function ow.item:Get(look)
