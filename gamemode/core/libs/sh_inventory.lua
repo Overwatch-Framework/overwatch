70eb6c8d25d2a270ee6636bd3e9aa2c659c60737
eon (bloodycop) <minerva.servers.eon@gmail.com>
2025-01-25 02:30:24 +0200

    Update sh_inventory.lua

--- gamemode/core/libs/sh_inventory.lua
+++ gamemode/core/libs/sh_inventory.lua
@@ -1,1 +1,13 @@
 ow.inventory = {}
+ow.inventory.stored = {}
+
+function ow.inventory.Get(index)
+    return ow.inventory.stored[index] 
+end
+
+function ow.inventory.Register(invData)
+    invData.index = #ow.inventory.stored + 1
+    ow.inventory.stored[invData.index] = invData
+
+    return invData.index
+end
