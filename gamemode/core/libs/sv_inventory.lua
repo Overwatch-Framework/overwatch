440c6867cf7ba16345a1735452a2740aed76418c
eon (bloodycop) <minerva.servers.eon@gmail.com>
2025-01-28 23:27:45 +0200

    Update sv_inventory.lua

--- gamemode/core/libs/sv_inventory.lua
+++ gamemode/core/libs/sv_inventory.lua
@@ -1,2 +1,8 @@
 --- Inventory library
 -- @module ow.inventory
+
+function ow.inventory:PerformAction(item, action) -- prob needs more args
+    if ( item == nil ) then return end
+
+    -- TODO: Implement
+end
