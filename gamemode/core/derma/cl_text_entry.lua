4039baee758858ae3a6f4dcbdbfe8a4f6a8b66cd
Riggs <riggs9162@gmx.de>
2025-04-27 01:19:13 +0200

    Add ripple effect to text entry panel
    Introduced a ripple effect on mouse press for the text entry panel using stencil masking. This enhances the visual feedback for user interactions.

--- gamemode/core/derma/cl_text_entry.lua
+++ gamemode/core/derma/cl_text_entry.lua
@@ -21,14 +21,67 @@ function PANEL:SizeToContents()
     self:SetSize(width + 8, height + 4)
 end
 
-local color = Color(0, 0, 0, 150)
-function PANEL:Paint(width, height)
-    paint.startVGUI()
-        paint.blur.requestBlur("default")
-        paint.roundedBoxes.roundedBox(8, 0, 0, width, height, color)
-    paint.endVGUI()
+local function mask(drawMask, draw)
+    render.ClearStencil()
+    render.SetStencilEnable(true)
 
+    render.SetStencilWriteMask(1)
+    render.SetStencilTestMask(1)
+
+    render.SetStencilFailOperation(STENCIL_REPLACE)
+    render.SetStencilPassOperation( STENCIL_REPLACE)
+    render.SetStencilZFailOperation(STENCIL_KEEP)
+    render.SetStencilCompareFunction(STENCIL_ALWAYS)
+    render.SetStencilReferenceValue(1)
+
+    drawMask()
+
+    render.SetStencilFailOperation(STENCIL_KEEP)
+    render.SetStencilPassOperation(STENCIL_REPLACE)
+    render.SetStencilZFailOperation(STENCIL_KEEP)
+    render.SetStencilCompareFunction(STENCIL_EQUAL)
+    render.SetStencilReferenceValue(1)
+
+    draw()
+
+    render.SetStencilEnable(false)
+    render.ClearStencil()
+end
+
+local RIPPLE_DIE_TIME = 1
+local RIPPLE_START_ALPHA = 50
+
+function PANEL:Paint(width, height)
     BaseClass.Paint(self, width, height)
+
+    paint.startPanel(self)
+        mask(function()
+            paint.roundedBoxes.roundedBox(0, 0, 0, width, height, Color(0, 0, 0, 50))
+        end,
+        function()
+            local ripple = self.rippleEffect
+            if ( ripple == nil ) then return end
+
+            local rippleX, rippleY, rippleStartTime = ripple[1], ripple[2], ripple[3]
+
+            local percent = (RealTime() - rippleStartTime)  / RIPPLE_DIE_TIME
+            if ( percent >= 1 ) then
+                self.rippleEffect = nil
+            else
+                local alpha = RIPPLE_START_ALPHA * (1 - percent)
+                local radius = math.max(width, height) * percent * math.sqrt(2)
+
+                paint.roundedBoxes.roundedBox(0, rippleX - radius, rippleY - radius, radius * 2, radius * 2, ColorAlpha(self:GetTextColor(), alpha))
+            end
+        end)
+    paint.endPanel()
+end
+
+function PANEL:OnMousePressed(key)
+    BaseClass.OnMousePressed(self, key)
+
+    local posX, posY = self:LocalCursorPos()
+    self.rippleEffect = {posX, posY, RealTime()}
 end
 
 vgui.Register("ow.text.entry", PANEL, "DTextEntry")
