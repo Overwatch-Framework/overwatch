33aeb1a12f76a4e9c8eccddfab89b5b8f8a51b42
bloodycop <eon.bloodycop@gmail.com>
2025-04-28 02:40:06 +0300

    Refactor and document utility and shared modules
    Added detailed documentation for the `ow.type` table in `sh_util.lua` to improve clarity and maintainability. Refactored module loading logic in `shared.lua` for better readability and consistency, including indentation fixes and minor structural adjustments.

--- gamemode/core/sh_util.lua
+++ gamemode/core/sh_util.lua
@@ -1,6 +1,34 @@
 --- Utility functions
 -- @module ow.util
 
+--- A table of variable types that are used throughout the framework. It represents types as a table with the keys being the
+-- name of the type, and the values being some number value. **You should never directly use these number values!** Using the
+-- values from this table will ensure backwards compatibility if the values in this table change.
+--
+-- This table also contains the numerical values of the types as keys. This means that if you need to check if a type exists, or
+-- if you need to get the name of a type, you can do a table lookup with a numerical value. Note that special types are not
+-- included since they are not real types that can be compared with.
+-- @table ow.type
+-- @realm shared
+-- @field string A regular string.
+-- @field text A regular string that can contain newlines.
+-- @field number Any number.
+-- @field player Any player that matches the given query string in `ow.util.FindPlayer`.
+-- @field steamid A string that matches the Steam ID format of `STEAM_X:X:XXXXXXXX`.
+-- @field character Any player's character that matches the given query string in `ow.util.FindPlayer`.
+-- @field bool A string representation of a bool - `false` and `0` will return `false`, anything else will return `true`.
+-- @field color A color represented by its red/green/blue/alpha values.
+-- @field vector A 3D vector represented by its x/y/z values.
+-- @field optional This is a special type that can be bitwise OR'd with any other type to make it optional.
+-- @field array This is a special type that can be bitwise OR'd with any other type to make it an array of that type.
+-- @usage -- checking if type exists
+-- print(ow.type[2] != nil)
+-- > true
+--
+-- -- getting name of type
+-- print(ow.type[ow.type.string])
+-- > "string"
+
 ow.type = ow.type or {
     [2] = "string",
     [4] = "text",
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,34 +2,8 @@
 --- Top-level library containing all Overwatch libraries. A large majority of the framework is split into respective libraries that
 -- reside within `ow`.
 -- @module ow
+-- @author riggs9162 & bloodycop6385
 
---- A table of variable types that are used throughout the framework. It represents types as a table with the keys being the
--- name of the type, and the values being some number value. **You should never directly use these number values!** Using the
--- values from this table will ensure backwards compatibility if the values in this table change.
---
--- This table also contains the numerical values of the types as keys. This means that if you need to check if a type exists, or
--- if you need to get the name of a type, you can do a table lookup with a numerical value. Note that special types are not
--- included since they are not real types that can be compared with.
--- @table ow.type
--- @realm shared
--- @field string A regular string.
--- @field text A regular string that can contain newlines.
--- @field number Any number.
--- @field player Any player that matches the given query string in `ow.util.FindPlayer`.
--- @field steamid A string that matches the Steam ID format of `STEAM_X:X:XXXXXXXX`.
--- @field character Any player's character that matches the given query string in `ow.util.FindPlayer`.
--- @field bool A string representation of a bool - `false` and `0` will return `false`, anything else will return `true`.
--- @field color A color represented by its red/green/blue/alpha values.
--- @field vector A 3D vector represented by its x/y/z values.
--- @field optional This is a special type that can be bitwise OR'd with any other type to make it optional.
--- @field array This is a special type that can be bitwise OR'd with any other type to make it an array of that type.
--- @usage -- checking if type exists
--- print(ow.type[2] != nil)
--- > true
---
--- -- getting name of type
--- print(ow.type[ow.type.string])
--- > "string"
 GM.Name = "Overwatch"
 GM.Author = "Riggs & bloodycop"
 GM.Description = "A roleplaying gamemode for Garry's Mod."
@@ -54,45 +28,43 @@ end
 
 
 ow.util:Print("Initializing...")
-ow.util:LoadFolder("core/libs")
-ow.util:LoadFolder("core/thirdparty")
-ow.util:LoadFolder("core/thirdparty/paint")
-ow.util:LoadFolder("core/meta")
-ow.util:LoadFolder("core/derma")
-ow.util:LoadFolder("core/hooks")
-ow.util:LoadFolder("core/net")
-ow.util:LoadFolder("core/languages")
+	ow.util:LoadFolder("core/libs")
+	ow.util:LoadFolder("core/thirdparty")
+	ow.util:LoadFolder("core/thirdparty/paint")
+	ow.util:LoadFolder("core/meta")
+	ow.util:LoadFolder("core/derma")
+	ow.util:LoadFolder("core/hooks")
+	ow.util:LoadFolder("core/net")
+	ow.util:LoadFolder("core/languages")
 ow.util:Print("Initialized.")
 
 ow.util:Print("Initializing Modules...")
+	local files, directories = file.Find("overwatch/modules/*", "LUA")
+	for k, v in ipairs(directories) do
+		if ( file.Exists("overwatch/modules/" .. v .. "/sh_module.lua", "LUA") ) then
+			MODULE = { UniqueID = v }
+				hook.Run("PreModuleLoad", v, MODULE)
+				ow.util:LoadFile("overwatch/modules/" .. v .. "/sh_module.lua")
+				ow.module.stored[v] = MODULE
+			MODULE = nil
+		else
+			ow.util:PrintError("Module " .. v .. " is missing a shared module file.")
+		end
+	end
 
-local files, directories = file.Find("overwatch/modules/*", "LUA")
-for k, v in ipairs(directories) do
-	if ( file.Exists("overwatch/modules/" .. v .. "/sh_module.lua", "LUA") ) then
-		MODULE = { UniqueID = v }
-			hook.Run("PreModuleLoad", v, MODULE)
-			ow.util:LoadFile("overwatch/modules/" .. v .. "/sh_module.lua")
-			ow.module.stored[v] = MODULE
+	for k, v in ipairs(files) do
+		local ModuleUniqueID = string.StripExtension(v)
+		if ( string.sub(v, 1, 3) == "cl_" or string.sub(v, 1, 3) == "sv_" or string.sub(v, 1, 3) == "sh_" ) then
+			ModuleUniqueID = string.sub(v, 4)
+		end
+
+		MODULE = { UniqueID = ModuleUniqueID }
+			hook.Run("PreModuleLoad", ModuleUniqueID, MODULE)
+			ow.util:LoadFile("overwatch/modules/" .. v, "shared")
+			ow.module.stored[ModuleUniqueID] = MODULE
+			hook.Run("PostModuleLoad", ModuleUniqueID, MODULE)
 		MODULE = nil
-	else
-		ow.util:PrintError("Module " .. v .. " is missing a shared module file.")
 	end
-end
-
-for k, v in ipairs(files) do
-	local ModuleUniqueID = string.StripExtension(v)
-	if ( string.sub(v, 1, 3) == "cl_" or string.sub(v, 1, 3) == "sv_" or string.sub(v, 1, 3) == "sh_" ) then
-		ModuleUniqueID = string.sub(v, 4)
-	end
-
-	MODULE = { UniqueID = ModuleUniqueID }
-		hook.Run("PreModuleLoad", ModuleUniqueID, MODULE)
-		ow.util:LoadFile("overwatch/modules/" .. v, "shared")
-		ow.module.stored[ModuleUniqueID] = MODULE
-		hook.Run("PostModuleLoad", ModuleUniqueID, MODULE)
-	MODULE = nil
-end
-
 ow.util:Print("Initialized Modules.")
 
 ow.reloaded = false
