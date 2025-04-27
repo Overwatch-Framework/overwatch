257d21b7c8d95cc59b7c064ff4b2d5524a81c16d
Riggs <riggs9162@gmx.de>
2025-04-28 01:54:43 +0200

    Add language options to game settings
    Introduced a predefined list of supported languages for the game, including English, German, French, and others. Updated the 'language' option to populate its values dynamically from this list.

--- gamemode/core/sh_options.lua
+++ gamemode/core/sh_options.lua
@@ -1,8 +1,42 @@
+-- Don't know if there is are more efficient way to do this or to retrieve the languages from gmod itself.
+local languages = {
+    ["en"] = "English",
+    ["de"] = "German",
+    ["fr"] = "French",
+    ["it"] = "Italian",
+    ["es"] = "Spanish (Spain)",
+    ["pt"] = "Portuguese (Portugal)",
+    ["pt-br"] = "Portuguese (Brazil)",
+    ["ru"] = "Russian",
+    ["pl"] = "Polish",
+    ["ja"] = "Japanese",
+    ["ko"] = "Korean",
+    ["zh-cn"] = "Chinese (Simplified)",
+    ["zh-tw"] = "Chinese (Traditional)",
+    ["da"] = "Danish",
+    ["nl"] = "Dutch",
+    ["fi"] = "Finnish",
+    ["no"] = "Norwegian",
+    ["sv"] = "Swedish",
+    ["tr"] = "Turkish",
+    ["uk"] = "Ukrainian",
+    ["cs"] = "Czech",
+    ["hu"] = "Hungarian",
+    ["ro"] = "Romanian",
+    ["bg"] = "Bulgarian",
+    ["el"] = "Greek",
+    ["th"] = "Thai",
+    ["vi"] = "Vietnamese"
+}
+
 ow.option:Register("language", {
     Name = "Language",
     Description = "The language of the game.",
-    Type = ow.type.string,
-    Default = "en"
+    Type = ow.type.array,
+    Default = "en",
+    Populate = function()
+        return languages
+    end,
 })
 
 ow.option:Register("vignette", {
