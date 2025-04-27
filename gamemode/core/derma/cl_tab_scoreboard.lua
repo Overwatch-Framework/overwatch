dffd542e38e3c8da1ee72c0cadb2df8e375bdd86
Riggs <riggs9162@gmx.de>
2025-04-27 20:01:50 +0200

    Add custom scoreboard UI
    Introduced a new scoreboard UI with panels for teams and players. The scoreboard dynamically updates based on the current players and organizes them by team, displaying relevant information such as player names, avatars, and ping.

---
+++ gamemode/core/derma/cl_tab_scoreboard.lua
@@ -0,0 +1,169 @@
+local gradientLeft = Material("vgui/gradient-l")
+local gradientBottom = Material("vgui/gradient-d")
+
+DEFINE_BASECLASS("EditablePanel")
+
+local PANEL = {}
+
+function PANEL:Init()
+    self:Dock(FILL)
+    self:DockMargin(0, 0, 0, 0)
+
+    self.cache = {}
+    self.cache.players = {}
+end
+
+function PANEL:Think()
+    if ( #self.cache.players != #player.GetAll() ) then
+        self:Populate()
+    end
+end
+
+function PANEL:Populate()
+    self.cache.players = player.GetAll()
+
+    -- Divide the players into teams
+    local teams = {}
+    for _, ply in ipairs(self.cache.players) do
+        local teamID = ply:Team()
+        if ( !teams[teamID] ) then
+            teams[teamID] = {}
+        end
+
+        table.insert(teams[teamID], ply)
+    end
+
+    -- Sort the teams by their team ID
+    local sortedTeams = {}
+    for teamID, players in pairs(teams) do
+        table.insert(sortedTeams, { teamID = teamID, players = players })
+    end
+    table.sort(sortedTeams, function(a, b) return a.teamID < b.teamID end)
+
+    -- Clear the current scoreboard
+    self:Clear()
+
+    for _, teamData in ipairs(sortedTeams) do
+        local teamID = teamData.teamID
+        local players = teamData.players
+
+        -- Create a new panel for the team
+        local teamPanel = self:Add("ow.tab.scoreboard.team")
+        teamPanel:SetTeam(teamID)
+
+        -- Add each player to the team panel
+        for _, ply in ipairs(players) do
+            local playerPanel = teamPanel.container:Add("ow.tab.scoreboard.player")
+            playerPanel:SetPlayer(ply)
+
+            teamPanel.players[ply:SteamID64()] = playerPanel
+        end
+    end
+
+    self:InvalidateLayout(true)
+end
+
+vgui.Register("ow.tab.scoreboard", PANEL, "EditablePanel")
+
+PANEL = {}
+
+function PANEL:Init()
+    self:Dock(TOP)
+    self:DockMargin(0, 0, 0, ScreenScale(8))
+
+    self.teamID = 0
+    self.players = {}
+
+    self.teamName = self:Add("ow.text")
+    self.teamName:SetTall(ScreenScale(8))
+    self.teamName:Dock(TOP)
+    self.teamName:DockMargin(8, 0, 0, 0)
+    self.teamName:SetFont("ow.fonts.default.italic.bold")
+
+    self.container = self:Add("DPanel")
+    self.container:Dock(FILL)
+    self.container:DockMargin(0, 0, 0, 0)
+    self.container.Paint = function(this, width, height)
+        surface.SetDrawColor(0, 0, 0, 200)
+        surface.DrawRect(0, 0, width, height)
+
+        surface.SetMaterial(gradientBottom)
+        surface.SetDrawColor(50, 50, 50, 200)
+        surface.DrawTexturedRect(0, 0, width, height)
+    end
+end
+
+function PANEL:SetTeam(teamID)
+    self.teamID = teamID
+
+    if ( IsValid(self.teamName) ) then
+        self.teamName:SetText(team.GetName(teamID))
+    end
+end
+
+function PANEL:PerformLayout(width, height)
+    -- Resize the panel height to fit the team name and the other players
+    local teamNameHeight = self.teamName:GetTall()
+    local containerHeight = 0
+
+    for _, playerPanel in pairs(self.players) do
+        containerHeight = containerHeight + playerPanel:GetTall()
+    end
+
+    self:SetTall(math.max(teamNameHeight, containerHeight) + ScreenScale(8))
+end
+
+function PANEL:Paint(width, height)
+    local color = team.GetColor(self.teamID)
+
+    surface.SetMaterial(gradientLeft)
+    surface.SetDrawColor(color.r, color.g, color.b, 200)
+    surface.DrawTexturedRect(0, 0, width, self.teamName:GetTall())
+end
+
+vgui.Register("ow.tab.scoreboard.team", PANEL, "EditablePanel")
+
+PANEL = {}
+
+function PANEL:Init()
+    self:Dock(TOP)
+    self:SetTall(ScreenScale(16))
+
+    self.avatar = self:Add("AvatarImage")
+    self.avatar:SetSize(self:GetTall(), self:GetTall())
+    self.avatar:SetPos(0, 0)
+
+    self.name = self:Add("ow.text")
+    self.name:SetFont("ow.fonts.default.large")
+
+    self.ping = self:Add("ow.text")
+    self.ping:SetSize(ScreenScale(32), self:GetTall())
+    self.ping:SetFont("ow.fonts.default")
+    self.ping:SetContentAlignment(6)
+end
+
+function PANEL:SetPlayer(ply)
+    self.player = ply
+
+    if ( IsValid(self.avatar) ) then
+        self.avatar:SetPlayer(ply, 128)
+    end
+
+    if ( IsValid(self.name) ) then
+        self.name:SetText(ply:SteamName())
+        self.name:SizeToContents()
+        self.name:SetPos(self.avatar:GetWide() + 16, self:GetTall() / 2 - self.name:GetTall() / 2)
+    end
+end
+
+function PANEL:Think()
+    if ( IsValid(self.ping) and IsValid(self.player) ) then
+        self.ping:SetText(self.player:Ping() .. "ms")
+        self.ping:SetPos(self:GetWide() - self.ping:GetWide() - 16, self:GetTall() / 2 - self.ping:GetTall() / 2)
+    end
+end
+
+function PANEL:Paint(width, height)
+end
+
+vgui.Register("ow.tab.scoreboard.player", PANEL, "EditablePanel")
