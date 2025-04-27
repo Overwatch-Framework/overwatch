af78f7bf9188b83f3dad065a872673a5582164e6
bloodycop <eon.bloodycop@gmail.com>
2025-04-28 02:22:28 +0300

    Create cl_mainmenu_load.lua

---
+++ gamemode/core/derma/cl_mainmenu_load.lua
@@ -0,0 +1,12 @@
+-- local padding = ScreenScale(32)
+-- local smallPadding = ScreenScale(16)
+
+DEFINE_BASECLASS("EditablePanel")
+
+local PANEL = {}
+
+function PANEL:Init()
+    self:SetSize(ScrW(), ScrH())
+    self:SetPos(0, 0)
+    self:SetVisible(false)
+end
