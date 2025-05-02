8714c9b922ea8e955fe346909fce42e284eb8fd8
Riggs <riggs9162@gmx.de>
2025-05-02 04:10:58 +0200

    Add logging for player config reset
    Introduced a new function `PostPlayerConfigReset` to log when a player resets a configuration key.

--- modules/logging/sv_hooks.lua
+++ modules/logging/sv_hooks.lua
@@ -89,3 +89,7 @@ function MODULE:PostPlayerConfigChanged(ply, key, value, oldValue)
         self:SendLog(ow.color:Get("yellow"), self:FormatPlayer(ply) .. " changed config " .. key .. " from " .. tostring(oldValue) .. " to " .. tostring(value))
     end
 end
+
+function MODULE:PostPlayerConfigReset(ply, key)
+    self:SendLog(ow.color:Get("yellow"), self:FormatPlayer(ply) .. " reset config " .. key)
+end
