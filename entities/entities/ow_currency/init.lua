f4cabee6a5e53206053afd0e74bfc1decb9e2cf0
bloodycop <eon.bloodycop@gmail.com>
2025-05-04 01:44:24 +0300

    Update init.lua

--- entities/entities/ow_currency/init.lua
+++ entities/entities/ow_currency/init.lua
@@ -14,7 +14,6 @@ end
 
 function ENT:Use(ply)
     if ( !IsValid(ply) or !ply:IsPlayer() ) then return end
-    if ( hook.Run("CanPlayerTakeMoney", ply, self) == false ) then return end
 
     local amount = self:GetAmount()
     if ( amount <= 0 ) then
@@ -22,17 +21,19 @@ function ENT:Use(ply)
         return
     end
 
+    if ( hook.Run("CanPlayerTakeMoney", ply, self, amount) == false ) then return end
+
     local character = ply:GetCharacter()
     if ( !character ) then return end
 
-    local prevent = hook.Run("PrePlayerTakeMoney", ply, self, amount)
-    if ( prevent == false ) then return end
+    hook.Run("PrePlayerTakeMoney", ply, self, amount)
 
     character:GiveMoney(amount)
     net.Start("ow.currency.give")
         net.WriteFloat(amount, 32)
         net.WriteEntity(self)
     net.Send(ply)
+
     hook.Run("PostPlayerTakeMoney", ply, self, amount)
 
     SafeRemoveEntity(self)
